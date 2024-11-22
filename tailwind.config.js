5cc792ddbae5146de16d58dd0282f4fadc25bf0f
flavius-constantin <flavius2105@gmail.com>
2024-11-22 18:58:33 +0100

    some text fixes & custom css animation & fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
     content: ["./**/*.{html,js}"],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                'sans': ['Inter', 'sans-serif'],
+            },
+            keyframes: {
+                animatedgradient: {
+                    '0%': { backgroundPosition: '0% 50%' },
+                    '50%': { backgroundPosition: '100% 50%' },
+                    '100%': { backgroundPosition: '0% 50%' },
+                },
+            },
+            backgroundSize: {
+                '300%': '300%',
+            },
+            animation: {
+                gradient: 'animatedgradient 6s ease infinite alternate',
+            },
+        },
     },
     plugins: [
         require('@tailwindcss/typography'),
